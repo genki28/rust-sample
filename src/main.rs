cc156a717d3851044c1ad738d08c051bcffcaecb
genki28 <taiiku39@gmail.com>
2022-01-04 02:08:47 +0900

    基本

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,12 @@ fn main() {
   let ns = format!("{} {} {}", s1, s2, s3);
   println!("{}", ns);
 
+  let t = ("genki", 30);
+  println!("name is {} age {}", t.0, t.1);
+
+  let a = ["春", "夏", "秋", "冬"];
+  println!("最初の季節は{}", a[0]);
+  println!("最後の季節は{}", a[3]);
 }
 
 fn add(x: i32, y: i32) -> i32 {
